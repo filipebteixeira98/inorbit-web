27d9528c724b11cd34f3157c120246a5d856ae0d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-20 02:35:29 -0300

    chore: fetch goals summary from api

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,36 @@
+import { useState, useEffect } from 'react'
+
 import { Dialog } from './components/ui/dialog'
 
 import { CreateGoal } from './components/create-goal'
-// import { EmptyGoals } from './components/empty-goals'
+import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
 
+type SummaryProps = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
+
 export function App() {
+  const [summary, setSummary] = useState<SummaryProps | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => response.json())
+      .then(data => setSummary(data.summary))
+  }, [])
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
