ee36d64d0088a676c381921faf43137d9cd92f4d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-20 02:50:55 -0300

    refactor: retrieved data from server using useQuery hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
 
 import { Dialog } from './components/ui/dialog'
 
@@ -6,31 +6,17 @@ import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
 
-type SummaryProps = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
+import { getSummary } from './http/get-summary'
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryProps | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then(response => response.json())
-      .then(data => setSummary(data.summary))
-  }, [])
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
 
   return (
     <Dialog>
-      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
