3bd305f6cc79dffa8674bfc7095aa17e906c09aa
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-01-02 18:42:42 -0300

    chore: create sign in page structure

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -8,7 +8,7 @@ import letsStart from '../assets/lets-start-illustration.svg'
 
 export function EmptyGoals() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
+    <main className="h-screen flex flex-col items-center justify-center gap-8">
       <img src={logo} alt="in.orbit" />
       <img src={letsStart} alt="in.orbit" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
@@ -16,14 +16,11 @@ export function EmptyGoals() {
         now?
       </p>
       <DialogTrigger asChild>
-        <Button
-          type="button"
-          className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-        >
+        <Button>
           <Plus className="size-4" />
           Register goal
         </Button>
       </DialogTrigger>
-    </div>
+    </main>
   )
 }
